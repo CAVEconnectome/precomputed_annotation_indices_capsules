80c742e5bd83762bf24278f5291fe0b21cf6d50d
kebwi <keith.wiley@alleninstitute.org>
2026-08-25 19:26:19 +0000

    Edited capsule_build_spatial_index_standalone.py

--- code/capsule_build_spatial_index_standalone/capsule_build_spatial_index_standalone.py
+++ code/capsule_build_spatial_index_standalone/capsule_build_spatial_index_standalone.py
@@ -421,6 +421,7 @@ def build_spatial_index(data_loc, results_loc, config, input_csv_path=None):
 
 
 def run(input_dir, output_dir, data_config, input_file):
+    logging.info(f"data_config: {data_config}")
     config = gc.init_config_w_data_config(data_config)
 
     logging.basicConfig(
