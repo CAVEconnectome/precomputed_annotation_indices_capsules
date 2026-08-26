cce8ee76ff4b4ca0edbb6d9faa441ae2f3e44921
kebwi <keith.wiley@alleninstitute.org>
2026-08-26 17:56:58 +0000

    Edited __init__.py

--- code/precomputed_annotation_indices/__init__.py
+++ code/precomputed_annotation_indices/__init__.py
@@ -1,6 +1,3 @@
-from capsule_build_spatial_index_standalone.capsule_build_spatial_index_standalone import (
-    build_spatial_index_standalone,
-    capsule_generate_config,
-    capsule_build_spatial_index_standalone,
-)
+import capsule_generate_config.capsule_generate_config as gc
+import capsule_build_spatial_index_standalone.capsule_build_spatial_index_standalone as bsis
 from shared import util, nested_profiler, annotations
